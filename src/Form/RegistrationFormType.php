670d9c5ab770be24fa07955f36dfb90f19b3c83b
Nerpp <regiaurelien@gmail.com>
2022-10-27 16:41:51 +0200

    before answer to comment

--- src/Form/RegistrationFormType.php
+++ src/Form/RegistrationFormType.php
@@ -123,7 +123,7 @@ class RegistrationFormType extends AbstractType
                 'mapped' => false,
                 'constraints' => [
                     new IsTrue([
-                        'message' => 'You should agree to our terms.',
+                        'message' => 'Vous devez accepter les termes d\'utilisation.',
                     ]),
                 ],
             ])
